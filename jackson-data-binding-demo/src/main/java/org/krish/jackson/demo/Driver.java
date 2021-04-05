088815ba9c87ceb3593760e9039dfe7a39f87d1c
Krish <krishnendu.gupta00@gmail.com>
2021-04-05 14:15:30 +0530

    Nested objects and arrays

--- jackson-data-binding-demo/src/main/java/org/krish/jackson/demo/Driver.java
+++ jackson-data-binding-demo/src/main/java/org/krish/jackson/demo/Driver.java
@@ -3,6 +3,7 @@ package org.krish.jackson.demo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Driver {
 
@@ -14,10 +15,14 @@ public class Driver {
             ObjectMapper mapper = new ObjectMapper();
 
             //Read JSON file and convert to Java POJO : sample-lite.json
-            Student student = mapper.readValue(new File(Driver.class.getClassLoader().getResource("sample-lite.json").getFile()),
-                                                Student.class);
+            /*Student student = mapper.readValue(new File(Driver.class.getClassLoader().getResource("sample-lite.json").getFile()),
+                                                Student.class);*/
+            Student_2 student = mapper.readValue(new File(Driver.class.getClassLoader().getResource("sample-full.json").getFile()),
+                    Student_2.class);
 
-            System.out.println(student);
+            System.out.println("Student Name : "+student.getFirstName()+" "+student.getLastName());
+            System.out.println("Address : "+ student.getAddress());
+            System.out.println("Languages: "+ Arrays.toString(student.getLanguages()));
 
         }catch (Exception e)
         {
